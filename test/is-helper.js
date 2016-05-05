26dc34a4ed8b605e49cc5ea4650b1d0c60cf4f44
Evan Lucas <evanlucas@me.com>
2016-05-05 13:16:21 -0500

    lib: add is-helper
    is-helper creates the ast needed for calling the is functions during
    validation.
    
    Ref: CHAT-397

---
+++ test/is-helper.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const test = require('tap').test
+const gen = require('../lib/generate').generate
+const is = require('../lib/is-helper')
+
+test('isDate', (t) => {
+  let varName = 'obj.createdAt'
+  let exp = 'validators.isDate(obj.createdAt)'
+  t.equal(gen(is.isDate(varName)), exp)
+
+  varName = 'obj'
+  exp = 'validators.isDate(obj)'
+  t.equal(gen(is.isDate(varName)), exp)
+  t.end()
+})
+
+test('isEmail', (t) => {
+  let varName = 'obj.email'
+  let exp = 'validators.isEmail(obj.email)'
+  t.equal(gen(is.isEmail(varName)), exp)
+
+  varName = 'obj'
+  exp = 'validators.isEmail(obj)'
+  t.equal(gen(is.isEmail(varName)), exp)
+  t.end()
+})
+
+test('isUUID', (t) => {
+  let varName = 'obj.id'
+  let exp = 'validators.isUUID(obj.id)'
+  t.equal(gen(is.isUUID(varName)), exp)
+
+  varName = 'obj'
+  exp = 'validators.isUUID(obj)'
+  t.equal(gen(is.isUUID(varName)), exp)
+  t.end()
+})
