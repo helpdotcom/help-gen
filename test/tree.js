2ec3d14c2a80a10645d0cf796b6cda095a796493
Anna Henningsen <anna@addaleax.net>
2016-11-19 23:30:05 +0100

    validator: allow iterating over all check instances
    Provide a `listPropTree()` method on the exported `File`
    class that returns a flattened list of all `Check` instances.
    
    Semver: minor

---
+++ test/tree.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const test = require('tap').test
+const Prop = require('@helpdotcom/nano-prop')
+const { Validator } = require('../')
+const { propSort } = require('../lib/utils')
+
+function flattenTreeForValidator(opts) {
+  const v = new Validator(opts)
+
+  return [...v.listPropTree()].map((check) => {
+    return {
+      type: check.prop.type
+    , displayPath: check.displayPath
+    , path: check.path
+    }
+  }).sort(propSort)
+}
+
+test('treeGenerator - basic', (t) => {
+  const flattenedTree = flattenTreeForValidator({
+    name: 'biscuits'
+  , multi: true
+  , type: 'test'
+  , props: [
+      Prop.string().path('str')
+    , Prop.array().path('arr').props([
+        Prop.number().path('num')
+      , Prop.boolean().path('bool')
+      ])
+    ]
+  })
+
+  t.deepEqual(flattenedTree, [
+    { type: 'array', displayPath: 'obj', path: 'obj' }
+  , { type: 'object', displayPath: 'obj[i]', path: 'obj.^__IDX__1' }
+  , { type: 'array', displayPath: 'arr', path: 'obj.^__IDX__1.arr' }
+  , { type: 'object'
+    , displayPath: 'arr[j]'
+    , path: 'obj.^__IDX__1.arr.^__IDX__2' }
+  , { type: 'boolean', displayPath: 'arr[j].bool'
+    , path: 'obj.^__IDX__1.arr.^__IDX__2.bool' }
+  , { type: 'number', displayPath: 'arr[j].num'
+    , path: 'obj.^__IDX__1.arr.^__IDX__2.num' }
+  , { type: 'string', displayPath: 'str', path: 'obj.^__IDX__1.str' }
+  ])
+
+  t.end()
+})
+
+test('treeGenerator - generic array', (t) => {
+  const flattenedTree = flattenTreeForValidator({
+    name: 'biscuits'
+  , multi: true
+  , type: 'test'
+  , props: [ Prop.array().path('arr') ]
+  })
+
+  t.deepEqual(flattenedTree, [
+    { type: 'array', displayPath: 'obj', path: 'obj' }
+  , { type: 'object', displayPath: 'obj[i]', path: 'obj.^__IDX__1' }
+  , { type: 'array', displayPath: 'arr', path: 'obj.^__IDX__1.arr' }
+  ])
+
+  t.end()
+})
+
+test('treeGenerator - generic object', (t) => {
+  const flattenedTree = flattenTreeForValidator({
+    name: 'biscuits'
+  , multi: true
+  , type: 'test'
+  , props: [ Prop.object().path('obj') ]
+  })
+
+  t.deepEqual(flattenedTree, [
+    { type: 'array', displayPath: 'obj', path: 'obj' }
+  , { type: 'object', displayPath: 'obj[i]', path: 'obj.^__IDX__1' }
+  , { type: 'object', displayPath: 'obj', path: 'obj.^__IDX__1.obj' }
+  ])
+
+  t.end()
+})
