71bd8c6039ef2e31c1c6d405bc7e85cd101b0ba6
Anna Henningsen <anna@addaleax.net>
2016-11-19 05:44:42 +0100

    validator: pass display paths for props explicitly
    Instead of inferring them from their `.path` properties, pass them
    explicitly. The goal here is decoupling the way of accessing a
    property in the generated code from the way a `Check` instance
    identifies itself.

--- test/utils.js
+++ test/utils.js
@@ -23,22 +23,6 @@ test('isValidIdentifier', (t) => {
   t.end()
 })
 
-test('cleanPath', (t) => {
-  const clean = utils.cleanPath
-  const tests = new Map([
-    ['a', 'a']
-  , ['fasdfds', 'fasdfds']
-  , ['fd.^__IDX__', 'fd']
-  , ['fd.^__IDX__.items.^__IDX__1', 'fd.items[i]']
-  , ['fd.^__IDX__.items.^__IDX__1.a.^__IDX__2', 'fd.items[i].a[j]']
-  ])
-
-  for (const [name, val] of tests) {
-    t.equal(clean(name), val, name)
-  }
-  t.end()
-})
-
 test('getIsFunctionName', (t) => {
   t.throws(() => { return utils.getIsFunctionName('foobar') },
            /Invalid type: "foobar"/)
