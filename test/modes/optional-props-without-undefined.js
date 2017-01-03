a3a228eef716d3e054b27adeec704702aeb68142
Anna Henningsen <anna@addaleax.net>
2017-01-03 01:02:23 +0100

    validator: more flexible handling of missing props
    Allow explicitly specifying that `undefined` is a valid value for
    optional properties.
    
    Semver: minor

---
+++ test/modes/optional-props-without-undefined.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const test = require('tap').test
+const Prop = require('@helpdotcom/nano-prop')
+const { compile } = require('../common')
+
+test('validator - missing props become undefined', {
+  skip: process.env.HELPGEN_STRIP_EXTRA_PROPS !== '1'
+      && 'stripping not enabled'
+}, (t) => {
+  const input = {
+    name: 'arraytest'
+  , type: 'test'
+  , props: [
+      Prop.object()
+          .path('a')
+          .optional()
+    ]
+  }
+
+  const fn = compile(input)
+
+  const tests = [
+    { input: {}
+    , output: { a: undefined }
+    , error: null
+    , name: 'empty object'
+    }
+  ]
+
+  for (const item of tests) {
+    t.test(item.name, (tt) => {
+      const valid = fn(item.input, (err, out) => {
+        if (item.error === null) {
+          tt.equal(err, null, 'does not fail')
+          tt.same(out, item.output, 'always defines property')
+          tt.equal(valid, true, 'returns true')
+        } else {
+          tt.type(err, Error)
+          tt.match(err.message, item.error)
+          tt.equal(err.code, 'EINVAL', 'err.code === \'EINVAL\'')
+          tt.equal(valid, false, 'returns false')
+        }
+        tt.end()
+      })
+    })
+  }
+
+  t.end()
+})
---
+++ test/modes/undefined-optional-props.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const test = require('tap').test
+const Prop = require('@helpdotcom/nano-prop')
+const { compile } = require('../common')
+
+test('validator - is desired, optional properties may be `undefined`', (t) => {
+  const input = {
+    name: 'arraytest'
+  , type: 'test'
+  , props: [
+      Prop.object()
+          .path('a')
+          .optional()
+    ]
+  , optionalsMayBeUndefined: true
+  }
+
+  const fn = compile(input)
+
+  const tests = [
+    { input: { a: undefined }
+    , output: { a: undefined }
+    , error: null
+    , name: 'empty object with single property'
+    }
+  ]
+
+  for (const item of tests) {
+    t.test(item.name, (tt) => {
+      const valid = fn(item.input, (err, out) => {
+        if (item.error === null) {
+          tt.equal(err, null, 'does not fail')
+          tt.same(out, item.output, 'output matches input')
+          tt.equal(valid, true, 'returns true')
+        } else {
+          tt.type(err, Error)
+          tt.match(err.message, item.error)
+          tt.equal(err.code, 'EINVAL', 'err.code === \'EINVAL\'')
+          tt.equal(valid, false, 'returns false')
+        }
+        tt.end()
+      })
+    })
+  }
+
+  t.end()
+})
