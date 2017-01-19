26d840ab82d3adccd380caec412513d66c575dd1
Evan Lucas <evanlucas@me.com>
2017-01-19 11:22:25 -0600

    object: fix allowNull for objects
    Previously, an object that was marked allowNull that had child props
    would not generate properly due to the allowNull being at the same
    level as the object checks. This change wraps all other checks for
    objects that allowNull inside a block that is only executed in the
    event the object is not null.
    
    Fixes: https://git.help.com/common-backend/help-gen/issues/25
    Semver: patch

---
+++ test/validators/complicated-object.js
@@ -0,0 +1,94 @@
+'use strict'
+
+const test = require('tap').test
+const Prop = require('@helpdotcom/nano-prop')
+const common = require('../common')
+const compile = common.compile
+
+test ('validator - complicated object with props', (t) => {
+  const input = {
+    name: 'objecttest'
+  , type: 'test'
+  , props: [
+      Prop.number().path('id')
+    , Prop.object().path('event_content').props([
+        Prop.string().path('searchable_content')
+      , Prop.object().path('formatted_content').allowNull().props([
+          Prop.object().path('entityMap')
+        , Prop.array().path('blocks').props(
+            Prop.object().props([
+              Prop.string().path('text')
+            , Prop.number().path('depth').min(0)
+            , Prop.string().path('key').optional()
+            ])
+          )
+        ])
+      ])
+    ]
+  }
+
+  const fn = compile(input)
+
+  const tests = [
+    { input: {}
+    , error: 'invalid param: "event_content". Expected object'
+    , name: 'empty object'
+    }
+  , { input: { event_content: {} }
+    , error: 'invalid param: "event_content.formatted_content". Expected object'
+    , name: 'empty event_content'
+    }
+  , {
+      input: {
+        event_content: {
+          formatted_content: null
+        }
+      }
+    , error: 'invalid param: "event_content.searchable_content". Expected strin'
+    , name: 'missing searchable_content with null formatted_content'
+    }
+  , {
+      input: {
+        event_content: {
+          formatted_content: {
+            blocks: []
+          }
+        , searchable_content: 'test'
+        }
+      }
+    , error: 'invalid param: "event_content.formatted_content.entityMap".'
+    , name: 'missing entityMap'
+    }
+  , {
+      input: {
+        event_content: {
+          formatted_content: {
+            blocks: []
+          , entityMap: {}
+          }
+        , searchable_content: 'test'
+        }
+      }
+    , error: 'invalid param: "id". Expected number'
+    , name: 'missing id'
+    }
+  ]
+
+  for (const item of tests) {
+    t.test(item.name, (tt) => {
+      const valid = fn(item.input, (err) => {
+        if (item.error === null) {
+          tt.equal(err, null, 'does not fail')
+          tt.equal(valid, true, 'returns true')
+        } else {
+          tt.type(err, Error)
+          tt.match(err.message, item.error)
+          tt.equal(err.code, 'EINVAL', 'err.code === \'EINVAL\'')
+          tt.equal(valid, false, 'returns false')
+        }
+        tt.end()
+      })
+    })
+  }
+  t.end()
+})
