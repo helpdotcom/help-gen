0c9079a2ae197dce8ffeacd886d199ca140cb1d0
Evan Lucas <evanlucas@me.com>
2017-03-03 08:10:14 -0600

    types: support allowName() option in email type
    This allows one to pass in an email in the format like 'Name <email>'.
    We sometimes want people to be able to specify their name for things
    like the from address of an email, etc.
    
    Ref: CHAT-2199
    Semver: minor

---
+++ test/types/email.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const test = require('tap').test
+const common = require('../common')
+
+const getProp = common.getProp('email')
+
+function compile(props) {
+  return common.compile({
+    name: 'email_type'
+  , type: 'email_type'
+  , props: [props]
+  })
+}
+
+const ERROR_MESSAGE = /invalid param: "email". Expected email/
+
+const cases = new Set([
+  getProp()
+, getProp().allowNull()
+, getProp().optional()
+, getProp().optional().allowNull()
+, getProp().allowName()
+, getProp().allowName().allowNull()
+, getProp().allowName().optional()
+, getProp().allowName().optional().allowNull()
+])
+
+for (const prop of cases) {
+  const fn = compile(prop)
+  const name = prop.toString()
+
+  test(name, (t) => {
+    if (prop._required) {
+      t.test('missing prop fails', (tt) => {
+        const valid = fn({}, (err) => {
+          tt.type(err, Error)
+          tt.match(err, ERROR_MESSAGE)
+          tt.equal(valid, false, 'returns false')
+          tt.end()
+        })
+      })
+    } else {
+      t.test('missing prop passes', (tt) => {
+        const valid = fn({}, (err) => {
+          tt.error(err)
+          tt.equal(valid, true, 'returns true')
+          tt.end()
+        })
+      })
+    }
+
+    if (prop._allowNull) {
+      t.test('null prop passes', (tt) => {
+        const valid = fn({
+          email: null
+        }, (err) => {
+          tt.error(err)
+          tt.equal(valid, true, 'returns true')
+          tt.end()
+        })
+      })
+    } else {
+      t.test('null prop fails', (tt) => {
+        const valid = fn({
+          email: null
+        }, (err) => {
+          tt.type(err, Error)
+          tt.match(err, ERROR_MESSAGE)
+          tt.equal(valid, false, 'returns false')
+          tt.end()
+        })
+      })
+    }
+
+    if (prop._allowName) {
+      t.test('email with name passes', (tt) => {
+        const valid = fn({
+          email: 'Evan Lucas <evan.lucas@help.com>'
+        }, (err) => {
+          tt.error(err)
+          tt.equal(valid, true, 'returns true')
+          tt.end()
+        })
+      })
+    } else {
+      t.test('email with name fails', (tt) => {
+        const valid = fn({
+          email: 'Evan Lucas <evan.lucas@help.com>'
+        }, (err) => {
+          tt.type(err, Error)
+          tt.match(err, ERROR_MESSAGE)
+          tt.equal(valid, false, 'returns false')
+          tt.end()
+        })
+      })
+    }
+
+    t.end()
+  })
+}
